d88662e713915567f55ba7fee97e33558f00f92b
Xebec19 <rohandeveloper106@gmail.com>
2025-10-28 21:34:20 +0530

    feat: added mongodb connection thru secrets

--- deployment/users-api/users-app.js
+++ deployment/users-api/users-app.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const userRoutes = require('./routes/user-routes');
+const userRoutes = require("./routes/user-routes");
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
@@ -19,7 +19,7 @@ app.use(userRoutes);
 
 app.use((err, req, res, next) => {
   let code = 500;
-  let message = 'Something went wrong.';
+  let message = "Something went wrong.";
   if (err.code) {
     code = err.code;
   }
@@ -30,12 +30,14 @@ app.use((err, req, res, next) => {
   res.status(code).json({ message: message });
 });
 
+console.log({ connectionURI: process.env.MONGODB_CONNECTION_URI });
+
 mongoose.connect(
   process.env.MONGODB_CONNECTION_URI,
   { useNewUrlParser: true },
   (err) => {
     if (err) {
-      console.log('COULD NOT CONNECT TO MONGODB!');
+      console.log("COULD NOT CONNECT TO MONGODB!");
     } else {
       app.listen(3000);
     }
